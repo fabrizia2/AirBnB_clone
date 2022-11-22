afaf89f590d66c427d576aa39cd86020825dae40
iakev <kirimikmwongera@gmail.com>
2022-11-22 07:51:18 +0000

    converted directories to python packages

---
+++ models/base_model.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+Module that defines all common attributes/methods for all other
+classes to inherit from
+"""
+from datetime import datetime
+
+
+class BaseModel:
+    """
+    Class that contains the common attributes/methods that all other
+    classes inherit from
+    """
+
+    def __init__(self):
+        """
+        Initialises BaseModel class with default arguments
+        """
+
+        self.id = uuid.uuid4()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """
+        Prints a friendly representation of BaseModel's instances
+        """
+
+        return f"{[type(self).__name__]} {(self.id)} {self.__dict__}"
+
+    def save(self):
+        """
+        Updates public instance attribute 'updated_at'
+        """
+
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        returns a dictionary with all key/values of the instance's __dict__
+        """
+
+        dic = self.__dict__.copy()
+        dic["__class__"] = type(self).__name__
+        dic["created_at"] = self.created_at.isoformat()
+        dic["updated_at"] = self.updated_at.isoformat()
