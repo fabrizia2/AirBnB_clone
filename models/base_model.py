b6bb1efce1d7a8e3bab982b9415a9d1bef0916c6
iakev <kirimikmwongera@gmail.com>
2022-11-23 14:18:16 +0000

    modified init magic function to be able to recreate an instance from a dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -13,14 +13,21 @@ class BaseModel:
     classes inherit from
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initialises BaseModel class with default arguments
         """
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if len(kwargs) != 0:
+            for k in kwargs:
+                if k == "created_at" or k == "updated_at":
+                    setattr(self, k, datetime.fromisoformat(kwargs[k]))
+                elif k != "__class__":
+                    setattr(self, k, kwargs[k])
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
