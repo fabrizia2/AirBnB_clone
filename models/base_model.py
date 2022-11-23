88812a374c351ef2b01cd5159c191460df8eaacc
iakev <kirimikmwongera@gmail.com>
2022-11-23 14:49:16 +0000

    added storage new method call on initialization

--- models/base_model.py
+++ models/base_model.py
@@ -29,6 +29,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """
