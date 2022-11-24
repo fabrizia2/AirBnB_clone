315a64816338b51649bd72e9687a6cb869c5ca45
iakev <kirimikmwongera@gmail.com>
2022-11-24 06:52:19 +0000

    made appropriate corrections to add proper file storage engines

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,7 +5,7 @@ Filestorage module
 import json
 
 
-class FileStorage():
+class FileStorage:
     """
     serializes instances to a JSON file and deserializes JSON file to instances
     """
@@ -16,29 +16,33 @@ class FileStorage():
     """Public instances"""
     def all(self):
         """returns the dictionary __objects"""
-        return self.__objects
+
+        return type(self).__objects
 
     def new(self, obj):
         """
         sets in __objects the obj with key <obj class name>.id
         """
-        key = f"{[type(obj).__name__]}.{obj.id}"
-        self.__objects[key] = obj
+
+        key = f"{type(obj).__name__}.{obj.id}"
+        type(self).__objects[key] = str(obj)
 
     def save(self):
         """
         serializes __objects to the JSON file
         """
-        obj_dict = {}
-        with open(FileStorage.__file_path, 'w') as write_file:
+
+        obj_dict = self.all()
+        with open(type(self).__file_path, 'w') as write_file:
             json.dump(obj_dict, write_file)
 
     def reload(self):
         """
         deserializes the JSON file to __objects
         """
+
         try:
             with open(self.__file_path, "r") as read_file:
-                dict_obj = json.load(read_file)
+                type(self).__objects = json.load(read_file)
         except FileNotFoundError:
             pass
