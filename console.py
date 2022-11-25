066ff2eb01084bfcca93e78eae5a0ed6aacd4251
fabrizia2 <melvinrenish56@gmail.com>
2022-11-25 10:11:34 +0000

    Added the create function

--- console.py
+++ console.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """The console that contains the entry point of the command interpreter"""
 import cmd
-
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb)'
@@ -19,11 +19,17 @@ class HBNBCommand(cmd.Cmd):
         print("")
         return True
 
-    def emptyLine(self):
+    def emptyline(self):
         """
         overrides the ENTER and empty line
        """
         pass
+
+    def do_create(self, arg):
+        if arg == "BaseModel":
+            new_instance = BaseModel()
+            new_instance.save()
+            print(new_instance.id)
 
 
 if __name__ == '__main__':
