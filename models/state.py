9ae51b81f8900e559b62f3b093c99993ab9a37e6
iakev <kirimikmwongera@gmail.com>
2022-11-26 21:52:44 +0000

    corrected str magic function

--- models/state.py
+++ models/state.py
@@ -16,7 +16,7 @@ class State(BaseModel):
 
     def __str__(self):
         super().__str__()
-        return f"{[type(self).__name__]} ({self.id}) {self.__dict__}"
+        return f"[State] ({self.id}) {self.__dict__}"
 
     def save(self):
         super().save()
