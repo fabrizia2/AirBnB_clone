90d042534bd144b7037f1252822a82191af94935
iakev <kirimikmwongera@gmail.com>
2022-11-28 19:13:57 +0000

    made some pycodestyle corrections

--- models/amenity.py
+++ models/amenity.py
@@ -9,19 +9,34 @@ class Amenity(BaseModel):
     """
     public attributes
     """
-
     name = ""
+
     def __init__(self, *args, **kwargs):
+        """
+        Initialisation function from BaseModel
+        """
+
         super().__init__(self, *args, **kwargs)
 
     def __str__(self):
-        super().__str__()
+        """
+        User frriendly string representation of Ameninty
+        object
+        """
+
         return f"[Amenity] ({self.id}) {self.__dict__}"
 
     def save(self):
+        """
+        Save method for Amenity objects
+        """
         super().save()
 
     def to_dict(self):
+        """
+        returns a dictionary representaion of the object
+        """
         amenity_dict = super().to_dict()
         amenity_dict["__class__"] = type(self).__name__
         amenity_dict["name"] = type(self).name
+        return amenity_dict
